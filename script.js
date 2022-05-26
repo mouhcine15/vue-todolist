3707dbc420c5fa6ecbb4148656d6cc5b7a4994f9
mouhcine <mouhcine15.00@gmail.com>
2022-05-26 16:33:00 +0200

    milestone 1

---
+++ script.js
@@ -0,0 +1,27 @@
+const root = new Vue({
+    el: "#app",
+    data: {
+        todo: [
+            {
+                text: "Fare i compiti",
+                done:  true
+            },
+            {
+                text: "Fare la spesa",
+                done:  false
+            },
+            {
+                text: "Fare il bucato",
+                done:  true
+            }
+        ]
+    },
+    // milestone 1
+    methods: {
+        controlla(todo) {
+            if (todo.done == true) {
+                return "text-decoration-line-through";
+            }
+        }
+    }
+})
